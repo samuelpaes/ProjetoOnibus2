180178f39f23ac9cb18f2e4ed3e866a7f29cfa77
samuelpaes <samuelpaes@hotmail.com>
2016-10-14 22:46:04 -0300

    Atualização da Home
    Inclusão das consultas na Controller Principal

--- web_TCC/web_TCC/Controllers/PrincipalController.cs
+++ web_TCC/web_TCC/Controllers/PrincipalController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web_TCC.Models;
 
 namespace web_TCC.Controllers
 {
     public class PrincipalController : Controller
     {
+        private web_TCCContext db = new web_TCCContext();
         // GET: Principal
         public ActionResult Index()
         {
+            int totalUsuarios = (from p in db.Conta select p).Count();
+            TempData["Total de Usuários"] = totalUsuarios;
+
+            int totalLinhas = (from p in db.Linhas select p).Count();
+            TempData["Total de Linhas"] = totalLinhas;
+
+            int totalPontos = (from p in db.Pontos select p).Count();
+            TempData["Total de Pontos"] = totalPontos;
+
+            int totalRegistros = (from p in db.Registros select p).Count();
+            TempData["Total de Registros"] = totalRegistros;   
             return View();
         }
     }
